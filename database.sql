dd8832f122d62bd0bf717b2468e42c84850c5ac0
Vadim <vadimyaskiv89@gmail.com>
2024-05-26 18:38:44 +0300

    Step 6 ready to deploy

--- database.sql
+++ database.sql
@@ -1,7 +1,18 @@
 DROP TABLE IF EXISTS urls;
+DROP TABLE IF EXISTS urls_checks;
 
 CREATE TABLE urls(
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(255) NOT NULL UNIQUE,
     created_at timestamp NOT NULL
     );
+
+CREATE TABLE urls_checks(
+id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+url_id bigint REFERENCES urls (id),
+response_code int,
+h1 varchar(255),
+title varchar(255),
+description varchar(255),
+created_at date NOT NULL
+);
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -12,7 +12,9 @@ import psycopg2
 from psycopg2.extras import NamedTupleCursor
 import datetime
 import validators
-from urllib.parse import urlparse, urlunparse 
+from urllib.parse import urlparse, urlunparse
+import requests
+from bs4 import BeautifulSoup
 
 
 app = Flask(__name__)
@@ -76,17 +78,80 @@ def get_url_aftr_add(id):
             "SELECT * FROM urls WHERE id=%s;",(id,)
             )
         url_record = curs.fetchone()
-    return render_template('url.html', url=url_record)
+        curs.execute(
+            "SELECT * FROM urls_checks WHERE url_id=%s;", (id, )
+        )
+        checks = curs.fetchall()
+    return render_template('url.html', url=url_record, checks=checks)
 
 
 @app.get("/urls>")
 def get_all_urls():
     with connection.cursor(cursor_factory=NamedTupleCursor) as curs:
         curs.execute(
-            "SELECT * FROM urls ORDER BY urls.id;"
+            '''
+            SELECT DISTINCT ON (u.id)
+            u.id, u.name, uch.created_at, uch.response_code
+            FROM urls AS u
+            LEFT JOIN urls_checks as uch ON u.id = uch.url_id
+            ORDER BY u.id DESC, uch.id DESC;
+            '''
             )
         urls = curs.fetchall()
     return render_template('urls.html', urls=urls)
+
+
+@app.post("/urls/<int:id>/checks")
+def check_url(id):
+    with connection.cursor(cursor_factory=NamedTupleCursor) as curs:
+        curs.execute(
+            "SELECT * FROM urls WHERE id=%s;",(id,)
+            )
+        url = curs.fetchone()
+        content = get_content(url.name)
+        if not content:
+            flash('Произошла ошибка при проверке', 'danger')
+        else:
+            curs.execute(
+                '''
+                INSERT INTO urls_checks
+                (url_id, response_code, h1, title, description, created_at)
+                VALUES (%s, %s, %s, %s, %s, %s);
+                ''',
+                (
+                    id,
+                    content['response_code'],
+                    content['h1'],
+                    content['title'],
+                    content['description'],
+                    datetime.datetime.now(),
+                )
+            )
+            connection.commit()
+            flash('Страница успешно проверена', 'success')
+    return redirect(url_for('get_url_aftr_add', id=id))
+
+
+
+# function for getting data from site for urls_checks db table
+def get_content(url):
+    try:
+        response = requests.get(url)
+    except requests.exceptions.RequestException:
+        return False
+    
+    soup = BeautifulSoup(response.text, 'html.parser')
+    h1 = soup.find('h1')
+    title = soup.find('title')
+    description = soup.find('meta', attrs={'name': 'description'})
+
+    site_content = {
+        'response_code': response.status_code,
+        'h1': h1.get_text() if h1 else '',
+        'title': title.get_text() if title else '',
+        'description': description['content'] if description else ''
+        }
+    return site_content
 
 
 # function for validate URL
